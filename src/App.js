5ba11a05944e736cd1e14293a3c37c1c736b419d
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-10-21 12:38:54 -0700

    pulling data from prompt api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  async fetchData(url) {
+    try {
+      let res = await fetch(url, {
+        headers: {
+          Authorization: `Token 9e70f6d0114903e346004714595e80c5c7fdc3dc`
+        }
+      });
+      const data = await res.json();
+      console.log(data);
+      this.setState(prevState => ({ data: [...prevState.data, data] }));
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData(
+      `https://content-staging.prompt.com/api/data/university/171100/?identifierType=iped`
+    );
+    this.fetchData(
+      `https://content-staging.prompt.com/api/data/university/131496/?identifierType=iped`
+    );
+    this.fetchData(
+      `https://content-staging.prompt.com/api/data/university/98765/?identifierType=iped`
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.data.length > 0
+          ? this.state.data.map(uni => {
+              return <h2 key={uni.name}>{uni.name}</h2>;
+            })
+          : `Loading...`}
+      </div>
+    );
+  }
 }
 
 export default App;
